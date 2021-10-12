677c57036a2112da0c03c079faf2806dc8ff67be
Theray070696 <theray070696@gmail.com>
2021-10-12 16:27:00 -0400

    Upload Wallpaper-Prank.ps1

---
+++ Pranks/Wallpaper-Prank.ps1
@@ -0,0 +1,125 @@
+#########################################################################################################################
+# Minimizes all windows, takes a screenshot of the desktop, hides desktop icons, then sets wallpaper to the screenshot.
+# Great for people that leave their computers unlocked when walking away!
+# Written by Theray070696. My other scripts can be found at https://www.github.com/Theray070696/Powershell-Scripts
+#########################################################################################################################
+
+Function Set-WallPaper {
+ 
+<#
+ 
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
+    
+    .PARAMETER Image
+    Provide the exact path to the image
+ 
+    .PARAMETER Style
+    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+  
+    .EXAMPLE
+    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+  
+#>
+ 
+param (
+    [parameter(Mandatory=$True)]
+    # Provide path to image
+    [string]$Image,
+    # Provide wallpaper style that you would like applied
+    [parameter(Mandatory=$False)]
+    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+    [string]$Style
+)
+ 
+$WallpaperStyle = Switch ($Style) {
+  
+    "Fill" {"10"}
+    "Fit" {"6"}
+    "Stretch" {"2"}
+    "Tile" {"0"}
+    "Center" {"0"}
+    "Span" {"22"}
+  
+}
+ 
+If($Style -eq "Tile") {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+ 
+}
+Else {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+ 
+}
+ 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+#############################################################################
+# Minimize all open windows
+#############################################################################
+$shell = New-Object -ComObject "Shell.Application"
+$shell.minimizeall()
+
+sleep 2
+
+#############################################################################
+# Capturing a screenshot
+#############################################################################
+$File = "C:\Scripts\Wallpaper.bmp"
+Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
+# Gather Screen resolution information
+$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$Width = $Screen.Width
+$Height = $Screen.Height
+$Left = $Screen.Left
+$Top = $Screen.Top
+# Create bitmap using the top-left and bottom-right bounds
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+# Create Graphics object
+$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+# Capture screen
+$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+# Save to file
+$bitmap.Save($File)
+Write-Output "Screenshot saved to:"
+Write-Output $File
+
+
+#############################################################################
+# Hide desktop icons
+#############################################################################
+$Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+Set-ItemProperty -Path $Path -Name "HideIcons" -Value 1
+Get-Process "explorer"| Stop-Process
+
+#############################################################################
+# Set wallpaper
+#############################################################################
+Set-Wallpaper -Image "C:\Scripts\Wallpaper.bmp" -Style Span

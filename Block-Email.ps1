340e6814359d929795b14b2061b8083a96595aba
Theray070696 <theray070696@gmail.com>
2022-04-08 09:19:26 -0400

    Update Block-Email to work on folders of Eml files
    If you need me, I'll be going through 205 emails and reblocking them.

--- Block-Email.ps1
+++ Block-Email.ps1
@@ -25,6 +25,9 @@ function Block-Email
     .PARAMETER EmlFileName
         A string representing the eml file to parse.
 
+	.PARAMETER EmlFolder
+		A folder containing multiple eml files to parse.
+
     .EXAMPLE
         PS C:\> Block-Email -EmlFileName 'C:\Test\test.eml'
 
@@ -34,10 +37,57 @@ function Block-Email
     [CmdletBinding()]
     param
     (
-        [ValidateNotNullOrEmpty()]
         [string]
-        $EmlFileName
+        $EmlFileName,
+		
+		[string]
+		$EmlFolder
     )
+	
+	function ConvertEmlAndBlock($FilePath)
+	{
+		# Convert the EML file to a format we can parse.
+		$ConvertedEML = Convert-EmlFile -EmlFileName $FilePath
+
+		# Grab the From property from the converted file.
+		$From = Select-Object -InputObject $ConvertedEML -Property From
+
+		# Run regex.
+		if($From -match '\<([^\<]*)\>')
+		{
+			# Save the first result to a variable.
+			$fromText = $Matches[1]
+
+			Write-Host Email Address is $fromText.
+
+			# Block the sender in Office 365
+			Add-BlockedSender -SenderAddress $fromText
+
+			Write-Host Blocked $fromText.
+			
+			# Check what the sender domain is, and ask if the user wants to block it if it's not from email providers such as gmail.
+			$SenderDomain = $fromText.Split('@')[1]
+			
+			Write-Host $($KnownProviders -contains $SenderDomain)
+			
+			if(-not ($KnownProviders -contains $SenderDomain))
+			{
+				$Confirmation = Read-Host "Email domain $SenderDomain is not in known common email provider list, recommend blocking it if it's not recognized. Block? [y/N]"
+				
+				if($Confirmation -eq 'y')
+				{
+					Write-Host "Blocking $SenderDomain."
+					
+					Add-BlockedSender -SenderDomain $SenderDomain
+					
+					Write-Host "Blocked $SenderDomain."
+				}
+			}
+
+			# Remove the EML File so it's not grabbed next time.
+			Remove-Item $FilePath
+		}
+	}
 
     if([string]::IsNullOrEmpty($EmlFileName))
     {
@@ -46,6 +96,17 @@ function Block-Email
             $EmlFileName = $EMLSaveLocation
         }
     }
+	
+	if(![string]::IsNullOrEmpty($EmlFolder) -and [string]::IsNullOrEmpty($EmlFileName))
+	{
+		# We got a folder, loop through all the Eml files.
+		$EmlFiles = Get-ChildItem -Path $EmlFolder -Filter *.eml -File
+		
+		ForEach($EmlFile in $EmlFiles)
+		{
+			ConvertEmlAndBlock $EmlFile
+		}
+	}
 
     if([string]::IsNullOrEmpty($EmlFileName))
     {
@@ -53,45 +114,5 @@ function Block-Email
         return
     }
     
-    # Convert the EML file to a format we can parse.
-    $ConvertedEML = Convert-EmlFile -EmlFileName $EmlFileName
-
-    # Grab the From property from the converted file.
-    $From = Select-Object -InputObject $ConvertedEML -Property From
-
-    # Run regex.
-    if($From -match '\<([^\<]*)\>')
-    {
-        # Save the first result to a variable.
-        $fromText = $Matches[1]
-
-        Write-Host Email Address is $fromText.
-
-        # Block the sender in Office 365
-        Add-BlockedSender -SenderAddress $fromText
-
-        Write-Host Blocked $fromText.
-        
-        # Check what the sender domain is, and ask if the user wants to block it if it's not from email providers such as gmail.
-        $SenderDomain = $fromText.Split('@')[1]
-        
-        Write-Host $($KnownProviders -contains $SenderDomain)
-        
-        if(-not ($KnownProviders -contains $SenderDomain))
-        {
-            $Confirmation = Read-Host "Email domain $SenderDomain is not in known common email provider list, recommend blocking it if it's not recognized. Block? [y/N]"
-            
-            if($Confirmation -eq 'y')
-            {
-                Write-Host "Blocking $SenderDomain."
-                
-                Add-BlockedSender -SenderDomain $SenderDomain
-                
-                Write-Host "Blocked $SenderDomain."
-            }
-        }
-
-        # Remove the EML File so it's not grabbed next time.
-        Remove-Item $EmlFileName
-    }
+    ConvertEmlAndBlock $EmlFileName
 }

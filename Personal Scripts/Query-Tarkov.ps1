d615280f0b3197707c89fb090660e56c640a7641
Theray070696 <theray070696@gmail.com>
2022-07-05 19:21:11 -0400

    Updated Tarkov query script
    Made it output which vendor is best to sell to (flea market excluded)
    Converted tabs to spaces

--- Personal Scripts/Query-Tarkov.ps1
+++ Personal Scripts/Query-Tarkov.ps1
@@ -14,25 +14,54 @@ $HideoutQuery = "{ `"query`": `"{ hideoutStations { name levels { level itemRequ
 $MatchingUpgrades = [System.Collections.ArrayList]@()
 Function Check-Hideout($Name)
 {
-	$LowerName = $Name.ToLower()
-	
-	ForEach($Station in $HideoutResponseObj.data.hideoutStations)
-	{
-		$StationName = $Station.name
-		ForEach($Level in $Station.levels)
-		{
-			$LevelNum = $Level.level
-			ForEach($ItemRequirement in $Level.itemRequirements)
-			{
-				if($ItemRequirement.item.name.ToLower() -eq $LowerName)
-				{
+    $LowerName = $Name.ToLower()
+    
+    ForEach($Station in $HideoutResponseObj.data.hideoutStations)
+    {
+        $StationName = $Station.name
+        ForEach($Level in $Station.levels)
+        {
+            $LevelNum = $Level.level
+            ForEach($ItemRequirement in $Level.itemRequirements)
+            {
+                if($ItemRequirement.item.name.ToLower() -eq $LowerName)
+                {
                     Write-Verbose "Found match in $StationName $LevelNum"
-					$null = $MatchingUpgrades.Add("$StationName $LevelNum")
-					break
-				}
-			}
-		}
-	}
+                    $null = $MatchingUpgrades.Add("$StationName $LevelNum")
+                    break
+                }
+            }
+        }
+    }
+}
+
+Function Check-VendorPrice($ItemResponseObject)
+{
+    $HighestPrice = 0
+    $Seller = ''
+    
+    ForEach($Sell in $ItemResponseObj.data.itemsByName[0].sellFor)
+    {
+        if($Sell.vendor.name.ToLower() -eq 'flea market')
+        {
+            continue
+        }
+        
+        if($Sell.price -gt $HighestPrice)
+        {
+            $HighestPrice = $Sell.price
+            $Seller = $Sell.vendor.name
+            Write-Verbose "Current highest seller: $Seller for $HighestPrice"
+        }
+    }
+
+    if($HighestPrice -gt 0)
+    {
+        Write-Verbose "$Seller for $HighestPrice"
+        return "$Seller for $HighestPrice"
+    }
+
+    return ''
 }
 
 Write-Verbose 'Querying hideout data...'
@@ -43,76 +72,83 @@ $HideoutOK = $true
 
 if($HideoutResponse.StatusDescription -eq "OK")
 {
-	$HideoutResponseObj = ConvertFrom-Json $HideoutResponse
+    $HideoutResponseObj = ConvertFrom-Json $HideoutResponse
 } else
 {
-	$HideoutOK = $false
+    $HideoutOK = $false
 }
 
 if(-not $HideoutOK)
 {
-	Write-Host 'Could not retreive hideout info, will not be able to provide anything related to hideout'
+    Write-Host 'Could not retreive hideout info, will not be able to provide anything related to hideout'
 } else
 {
-	Write-Verbose 'Hideout OK!'
+    Write-Verbose 'Hideout OK!'
 }
 
 while($true)
 {
-	$ItemNameInput = Read-Host "Enter part of the item name you're checking or type 'exit' to exit"
-	
-	if([string]::IsNullOrEmpty($ItemNameInput))
-	{
-		Write-Error 'Invalid input'
-		continue
-	}
-	
-	if($ItemNameInput.ToLower() -eq 'exit' -or $ItemNameInput.ToLower() -eq 'quit')
-	{
+    $ItemNameInput = Read-Host "Enter part of the item name you're checking or type 'exit' to exit"
+    
+    if([string]::IsNullOrEmpty($ItemNameInput))
+    {
+        Write-Error 'Invalid input'
+        continue
+    }
+    
+    if($ItemNameInput.ToLower() -eq 'exit' -or $ItemNameInput.ToLower() -eq 'quit')
+    {
         Write-Verbose 'Exiting'
-		return
-	}
+        return
+    }
 
-	$ItemQuery = "{ `"query`": `"{ itemsByName(name: \`"$ItemNameInput\`") { name shortName id usedInTasks { name } } }`" }"
+    $ItemQuery = "{ `"query`": `"{ itemsByName(name: \`"$ItemNameInput\`") { name shortName id usedInTasks { name } sellFor { vendor { name } price } } }`" }"
 
     Write-Verbose "Query: $ItemQuery"
 
-	$ItemResponse = Invoke-WebRequest -Uri $API -Method 'POST' -ContentType 'application/json' -Body $ItemQuery
+    $ItemResponse = Invoke-WebRequest -Uri $API -Method 'POST' -ContentType 'application/json' -Body $ItemQuery
 
-	if($ItemResponse.StatusDescription -eq "OK")
-	{
-		$ItemResponseObj = ConvertFrom-Json $ItemResponse
-		if(-not [string]::IsNullOrEmpty($ItemResponseObj.data.itemsByName[0].name))
-		{
-			$ItemName = $ItemResponseObj.data.itemsByName[0].name
-			if($HideoutOK)
-			{
-				Write-Verbose "Searching hideout stations for $ItemName"
-				$MatchingUpgrades = [System.Collections.ArrayList]@()
-				Check-Hideout($ItemName)
-			}
-			
-			if(-not [string]::IsNullOrEmpty($ItemResponseObj.data.itemsByName[0].usedInTasks.Name))
-			{
-				Write-Host "$ItemName is used in the following tasks: $($ItemResponseObj.data.itemsByName[0].usedInTasks.Name -join ', ')"
-				
-				if($HideoutOK -and $MatchingUpgrades.Length -gt 0)
-				{
-					Write-Host "$ItemName is also used by the following hideout upgrades: $($MatchingUpgrades -join ', ')"
-				}
-			} elseif($HideoutOK -and $MatchingUpgrades.Length -gt 0)
-			{
-				Write-Host "$ItemName is used in the following hideout upgrades: $($MatchingUpgrades -join ', ')"
-			} elseif($HideoutOK)
-			{
-				Write-Host "$ItemName is not used in any tasks or hideout upgrades"
-			} else
-			{
-				Write-Host "$ItemName is not used in any tasks"
-			}
-		} else
-		{
-			Write-Host "$ItemNameInput was not found"
-		}
-	}
+    if($ItemResponse.StatusDescription -eq "OK")
+    {
+        $ItemResponseObj = ConvertFrom-Json $ItemResponse
+        if(-not [string]::IsNullOrEmpty($ItemResponseObj.data.itemsByName[0].name))
+        {
+            $ItemName = $ItemResponseObj.data.itemsByName[0].name
+            if($HideoutOK)
+            {
+                Write-Verbose "Searching hideout stations for $ItemName"
+                $MatchingUpgrades = [System.Collections.ArrayList]@()
+                Check-Hideout($ItemName)
+            }
+            
+            if(-not [string]::IsNullOrEmpty($ItemResponseObj.data.itemsByName[0].usedInTasks.Name))
+            {
+                Write-Host "$ItemName is used in the following tasks: $($ItemResponseObj.data.itemsByName[0].usedInTasks.Name -join ', ')"
+                
+                if($HideoutOK -and $MatchingUpgrades.Length -gt 0)
+                {
+                    Write-Host "$ItemName is also used by the following hideout upgrades: $($MatchingUpgrades -join ', ')"
+                }
+            } elseif($HideoutOK -and $MatchingUpgrades.Length -gt 0)
+            {
+                Write-Host "$ItemName is used in the following hideout upgrades: $($MatchingUpgrades -join ', ')"
+            } elseif($HideoutOK)
+            {
+                Write-Host "$ItemName is not used in any tasks or hideout upgrades"
+            } else
+            {
+                Write-Host "$ItemName is not used in any tasks"
+            }
+            
+            $SellsFor = Check-VendorPrice($ItemResponseObj)
+
+            if(-not [string]::IsNullOrEmpty($SellsFor))
+            {
+                Write-Host "$ItemName sells best at $SellsFor"
+            }
+        } else
+        {
+            Write-Host "$ItemNameInput was not found"
+        }
+    }
 }

44c3c4da81e5f4d3c84a3390d82e5b5eee220a53
Theray070696 <theray070696@gmail.com>
2022-08-02 13:27:51 -0400

    Added ability to get membership for ALL groups in AzureAD
    Also did skipping the remainder of the script in a much smarter way

--- Get-MembersOfAADGroup.ps1
+++ Get-MembersOfAADGroup.ps1
@@ -1,74 +1,91 @@
-﻿######################################################################################################################
+######################################################################################################################
 # Saves membership of all matching groups in AzureAD to CSV file specified.
 # Written by Theray070696. My other scripts can be found at https://www.github.com/Theray070696/Powershell-Scripts
 ######################################################################################################################
 
 [CMDLetBinding()]
 param(
-    [Parameter(Mandatory=$True, HelpMessage='The full or partial name of the group you want the members of.')]
+    [Parameter(Mandatory=$False, HelpMessage='The full or partial name of the group you want the members of.')]
     [string]$GroupName,
     [Parameter(Mandatory=$True, HelpMessage='The path to the output CSV.')]
-    [string]$Output
+    [string]$Output,
+	[Parameter(Mandatory=$False, HelpMessage='If supplied, gets all groups and their members, outputting to the same CSV file.')]
+	[switch]$All
 )
 
-if($PSVersionTable.PSVersion.Major -eq 7)
+If($PSVersionTable.PSVersion.Major -eq 7)
 {
     Write-Error "This script is incompatible with PowerShell 7, and is only verified to work currently on PowerShell 5."
     $SkipRemainder = $True
-} else
+    
+    return
+} Else
 {
-    if(-Not (Get-Command Get-AzureADGroup -ea SilentlyContinue))
+    If(-Not (Get-Command Get-AzureADGroup -ea SilentlyContinue))
     {
         Write-Warning "This function requires a connection to AzureAD. Prompting Now."
-      
-        if(-Not (Get-Command Connect-AzureAD -ea SilentlyContinue))
+	  
+        If(-Not (Get-Command Connect-AzureAD -ea SilentlyContinue))
         {
             Write-Error "Could not find command to connect to AzureAD."
             Write-Error "Please run Install-Module -Name AzureAD in an administrator PowerShell window to install the required module."
             $SkipRemainder = $True
-        } else
+            
+            return
+        } Else
         {
             Connect-AzureAD
-        
-            if(-not (Get-Command Get-AzureADGroup -ea SilentlyContinue))
+		
+            If(-not (Get-Command Get-AzureADGroup -ea SilentlyContinue))
             {
-                Write-Warning "Could not connect to AzureAD. Verify credentials and try again later."
+  	            Write-Warning "Could not connect to AzureAD. Verify credentials and try again later."
                 $SkipRemainder = $True
+                
+                return
             }
         }
     }
 }
 
-if($SkipRemainder -ne $True)
+$MemberCollection = New-Object System.Collections.Generic.List[System.Object]
+
+if($All)
 {
-    $MemberCollection = New-Object System.Collections.Generic.List[System.Object]
+    $Groups = Get-AzureADGroup
+} elseif(-not [string]::IsNullOrEmpty($GroupName))
+{
+    $Groups = Get-AzureADGroup -SearchString $GroupName
+} else
+{
+    Write-Error "No group name provided, and -All wasn't specified. Aborting."
+    return
+}
 
-    ForEach($Group in Get-AzureADGroup -SearchString $GroupName)
+ForEach($Group in $Groups)
+{
+    Write-Verbose "Group name: $($Group.DisplayName)"
+
+    $GroupMembers = Get-AzureADGroupMember -ObjectId $Group.ObjectId
+
+    ForEach($Member in $GroupMembers)
     {
-        Write-Verbose "Group name: $($Group.DisplayName)"
-        
-        $GroupMembers = Get-AzureADGroupMember -ObjectId $Group.ObjectId
-        
-        ForEach($Member in $GroupMembers)
-        {
-            $Mem = New-Object PSObject
-            $Mem | Add-Member NoteProperty GroupName($Group.DisplayName)
-            $Mem | Add-Member NoteProperty DisplayName($Member.DisplayName)
-            $Mem | Add-Member NoteProperty UserPrincipalName($Member.UserPrincipalName)
-            $Mem | Add-Member NoteProperty UserType($Member.UserType)
+        $Mem = New-Object PSObject
+        $Mem | Add-Member NoteProperty GroupName($Group.DisplayName)
+        $Mem | Add-Member NoteProperty DisplayName($Member.DisplayName)
+        $Mem | Add-Member NoteProperty UserPrincipalName($Member.UserPrincipalName)
+        $Mem | Add-Member NoteProperty UserType($Member.UserType)
 
-            $MemberCollection.Add($Mem)
+        $MemberCollection.Add($Mem)
 
-            Write-Verbose "Added to MemberCollection: GroupName: $($Group.DisplayName) ObjectId: $($Member.ObjectId) DisplayName: $($Member.DisplayName) UserPrincipalName: $($Member.UserPrincipalName) UserType: $($Member.UserType)"
-        }
+        Write-Verbose "Added to MemberCollection: GroupName: $($Group.DisplayName) ObjectId: $($Member.ObjectId) DisplayName: $($Member.DisplayName) UserPrincipalName: $($Member.UserPrincipalName) UserType: $($Member.UserType)"
     }
+}
 
-    if($MemberCollection.Count -gt 0)
-    {
-        $MemberCollection | Export-CSV $Output -NoTypeInformation
-        Write-Host -f Green "`n*** Folder Permission Report Generated Successfully!***"
-    } else
-    {
-        Write-Host -f Red "Error Generating User Membership Report!"
-    }
+If($MemberCollection.Count -gt 0)
+{
+    $MemberCollection | Export-CSV $Output -NoTypeInformation
+    Write-Host -f Green "`n*** User Membership Report Generated Successfully!***"
+} Else
+{
+    Write-Host -f Red "Error Generating User Membership Report!"
 }
